63b81388312a43c8c3a19fbadfa16dbac56acc9e
RicardoBarrote <ricardoambscfc@gmail.com>
2024-06-05 14:48:40 -0300

    Third commit, creating abstract class Content, passing Course and Mentoring as child classes

--- src/entities/Curso.java
+++ src/entities/Curso.java
@@ -1,26 +1,10 @@
 package entities;
 
-public class Curso {
+public class Curso extends Conteudo {
 
-    private String titulo;
-    private String descricao;
+
     private int cargaHoraria;
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public int getCargaHoraria() {
         return cargaHoraria;
@@ -32,6 +16,11 @@ public class Curso {
 
     @Override
     public String toString() {
-        return "Titulo: " + titulo + ", descricao: " + descricao + ", carga horaria: " + cargaHoraria;
+        return "Titulo: " + super.getTitulo() + ", descricao: " + super.getDescricao() + ", carga horaria: " + cargaHoraria;
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
     }
 }
