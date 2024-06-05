3602f2e3669b87d9f9c44eb1e5e365396b6ec9a6
RicardoBarrote <ricardoambscfc@gmail.com>
2024-06-05 15:53:46 -0300

    Fourth commit, completing the classes in the entities package

---
+++ src/entities/Dev.java
@@ -0,0 +1,64 @@
+package entities;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void inscreverBootcamp(BootCamp bootCamp) {
+        conteudosInscritos.addAll(bootCamp.getConteudos());
+        bootCamp.getDevsInscritos().add(this);
+    }
+
+    public void progredir() {
+        Optional<Conteudo> conteudo = conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()) {
+            conteudosConcluidos.add(conteudo.get());
+            conteudosInscritos.remove(conteudo.get());
+        }
+        else {
+            System.err.println("Você não esta matriculado em nenhum conteudo");
+        }
+    }
+
+    public double calcularTotalXp() {
+        return conteudosConcluidos.stream()
+                .mapToDouble(conteudo -> conteudo.calcularXp())
+                .sum();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
+    }
+}
